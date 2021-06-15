7c7ad9e116e4a161f84d9398389bce664d0f0778
Albert R. Carnier Guedes <albert@teko.net.br>
2021-06-15 15:27:09 -0300

    Created a index.php for package.

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+          
+   <body>
+        <h1>nano Project</h1>
+
+        <p><a href="/nano_admin" >nano Admin</a></p>
+                                           
+   </body>
+</html>
